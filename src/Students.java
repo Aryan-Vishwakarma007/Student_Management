a325298d6ba39ae19854c442d2dfd7461d5a44b7
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-05-25 12:59:18 +0530

    class made

--- src/Students.java
+++ src/Students.java
@@ -1,3 +1,26 @@
 public class Students {
+       private String name;
+       private int age;
+       private String Address;
+       private String hobbies;
 
+       public void setinfo(String name,int age, String Address,String hobbies ){
+           this.name = name;
+           this.age = age;
+           this.Address =Address;
+           this.hobbies = "No Hobbies";
+       }
+
+        public int getAge() {
+           return this.age;
+        }
+        public String getAddress() {
+            return this.Address;
+        }
+        public String getHobbies() {
+            return this.hobbies;
+        }
+        public String  getName() {
+            return this.name;
+        }
 }
