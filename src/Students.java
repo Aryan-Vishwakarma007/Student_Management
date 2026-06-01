980a46c20c07f6168ed89ac0b7254eabe39f4d6b
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-01 18:27:27 +0530

    default no hobbies

--- src/Students.java
+++ src/Students.java
@@ -44,5 +44,7 @@
 //    }
 //}
 record Students(String name,int age, String Address,String hobbies){
-
+        Students(String name, int age, String Address){
+            this(name,age,Address, "NO hobbies");
+        }
 }
